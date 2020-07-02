e67a879d360b055950f791018280e450847e24e3
mohitha03 <deepajyothinath@gmail.com>
2020-07-02 15:44:33 +0530

    my first demo push

---
+++ src/test/java/MavenTest/mavenartifact/deepaclass.java
@@ -0,0 +1,26 @@
+package MavenTest.mavenartifact;
+
+public class deepaclass {
+
+
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("sdfdsfsf");
+		deepaclass.add(1,20);
+		System.out.println(deepaclass.add(1,20)+"dffffffffffffffff");
+	}
+
+	
+	
+	
+	 static int add (int a, int b) {
+		
+		int c;
+			
+	 c = a + b ;
+	 System.out.println(c+"aaaaaaaaaaaaaa");
+	return c;
+}
+}
